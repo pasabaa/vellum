860a65e3ab07a08b5e1196c06997329af8fe480a
pasabaa <sanchez.ballesteros.pablo@gmail.com>
2024-10-17 11:46:23 -0600

    add background image to main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,6 +12,8 @@ const enriqueta = Enriqueta({
 export default function Home() {
   return (
     <Container>
+      <Image className="absolute top-0 left-0 w-full h-full object-cover -z-10" alt="cover" src={'https://res.cloudinary.com/dvuqzso8k/image/upload/v1727287428/mundo-mitico/3348fba4-30bf-4cb0-90da-b75039f2d66e_ua9tll.png'} width={1920} height={1080}/>
+      <div className="bg-gradient-to-t from-neutral-950 via-neutral-950/80 to-neutral-950 absolute top-0 left-0 w-full h-full -z-10"/>
       <h1 className={`text-5xl text-white ${enriqueta?.className}`}>Vellum</h1>
       <div className="text-neutral-400 mt-4 space-y-4">
         <p>
